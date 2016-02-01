cb1a7d521450aba87fe11a74cd1720bdbe26a5c2
bharding2 <b.harding@gmail.com>
2016-02-01 15:14:50 -0800

    article template html and js constructor function

---
+++ scripts/portfolio.js
@@ -0,0 +1,8 @@
+var projects = [];
+
+function Project(newProj) {
+  this.title = newProj.title;
+  this.category = newProj.category;
+  this.publishedOn = newProj.publishedOn;
+  this.body = newProj.body;
+}
