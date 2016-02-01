f71bd27057c2379f10d9ac05653dd490478602bf
bharding2 <b.harding@gmail.com>
2016-02-01 15:47:19 -0800

    jqueryed some toHtml

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,6 +3,30 @@ var projects = [];
 function Project(newProj) {
   this.title = newProj.title;
   this.category = newProj.category;
+  this.repoUrl = newProj.repoUrl;
   this.publishedOn = newProj.publishedOn;
   this.body = newProj.body;
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('.template').clone();
+
+  $newProject.attr('data-category', this.category);
+
+  $newProject.find('h2').text(this.title);
+  $newProject.find('address a').attr('href', this.repoUrl);
+  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newProject.find('.projectBody').html(this.body);
+
+  $newProject.removeClass();
+
+  return $newProject;
+};
+
+projectData.forEach(function(newProj) {
+  projects.push(new Project(newProj));
+});
+
+projects.forEach(function (project) {
+  $('#projects').append(project.toHtml());
+});
---
+++ scripts/projectdata.js
@@ -0,0 +1,9 @@
+var projectData = [
+  {
+    title:        'Hoppr',
+    category:     '201d5',
+    repoUrl:      'https://github.com/bharding2/hoppr',
+    publishedOn:  '2016-01-29',
+    body:         'Here is info about the Project'
+  }
+];
