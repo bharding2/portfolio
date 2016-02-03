62da2c28aa63db6bf2cefbc2594ae211e5b0886a
bharding2 <b.harding@gmail.com>
2016-02-03 14:32:16 -0800

    refactored yesterday's code, added images and ipsum to projectdata, split css out into three files for easier navigation

--- scripts/portfolioview.js
+++ scripts/portfolioview.js
@@ -1,23 +1,20 @@
 var portfolioView = {};
 
 portfolioView.handleNav = function () {
-  $('nav a').on('click', function (event) {
-    $('#projects, #about').hide();
-    var $dataContent = $(this).data('content');
-    console.log($dataContent);
-    $('#' + $dataContent).show();
+  $('nav').on('click', '.tab', function (event) {
+    $('.tab_content').hide();
+    $('#' + $(this).data('content')).show();
   });
-
+  
   $('nav .tab:first').click();
 };
 
 portfolioView.setTeasers = function() {
   $('.project_body *:nth-of-type(n+2)').hide();
 
-  $('#projects a.read_on').on('click', function(event) {
+  $('#projects').on('click', 'a.read_on', function(event) {
     event.preventDefault();
-    var $projectBodyContent = $(this).prev().children();
-    $($projectBodyContent).show();
+    $(this).prev().children().show();
     $(this).hide();
   });
 };
@@ -25,9 +22,10 @@ portfolioView.setTeasers = function() {
 portfolioView.populateFilters = function() {
   $('article').each(function() {
     if (!$(this).hasClass('template')) {
-      val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category_filter option[value="' + val + '"]').length === 0) {
+      var $val = $(this).attr('data-category');
+      var optionTag = '<option value="' + $val + '">' + $val + '</option>';
+
+      if ($('#category_filter option[value="' + $val + '"]').length === 0) {
         $('#category_filter').append(optionTag);
       }
     }
@@ -37,9 +35,8 @@ portfolioView.populateFilters = function() {
 portfolioView.handleCategoryFilter = function() {
   $('#category_filter').on('change', function() {
     if ($(this).val()) {
-      var $val = $(this).val();
       $('article').hide();
-      $('article[data-category="' + $val + '"]').fadeIn();
+      $('article[data-category="' + $(this).val() + '"]').fadeIn();
     } else {
       $('article').show();
       $('.template').hide();
