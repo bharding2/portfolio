e11a2cd9fda7d290d890fc268cf64f8ed336cbd3
bharding2 <b.harding@gmail.com>
2016-02-17 15:03:17 -0800

    githubstuff

--- scripts/repo.js
+++ scripts/repo.js
@@ -3,21 +3,9 @@
   repos.all = [];
   //accesses all github repos sorted by last update and pushes them to repos.all array
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/bharding2/repos' +
-            '?sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        console.log(data);
-        repos.all = [];
-        data.forEach(function(ele) {
-          repos.all.push(ele);
-        });
-      }
-    }).done(function() {
-      if(callback) callback();
-    });
+    $.get('/github/users/bharding2/repos?sort=updated', function(data, message, xhr) {
+      repos.all = data;
+    }).done(callback);
   };
   //rudimentary sorting function
   repos.with = function(attr) {
